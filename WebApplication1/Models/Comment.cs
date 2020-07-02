4cecd3f1a68048d89f54387e96a78d20372245e2
ThomasELong <thomaselong12@gmail.com>
2020-07-02 11:31:04 -0500

    added comment and userprofile functionality

---
+++ WebApplication1/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+        [Required]
+        public int PostId { get; set; }
+
+        public Post Post { get; set; }
+    }
+}
